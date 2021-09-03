4611b2dcd78ed94a060f2d00f84dac01ef26327d
Pavan S Patil <psp3075@gmail.com>
2021-09-03 21:08:25 +0530

    fetch api success

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Card from './components/Card';
+import { fetchQuestions } from './API';
+import { Difficulty } from './API';
 
+const TOTAL_QUESTIONS=10;
 
 function App() {
+  const [loading,setLoading]=useState(false)
+  const [questions,setQuestions]=useState([])
+  const [number,setNumber]=useState(0)
+  const [userAnswers,setAnswers]=useState([])
+  const [score,setScore]=useState(0)
+  const [gameOver,setGameOver]=useState(true)
+
+  console.log(fetchQuestions(TOTAL_QUESTIONS,Difficulty.EASY))
+
+  const startTrivia=async()=>{}
+  
+  const checkAnswer=(e:React.MouseEvent<HTMLButtonElement>)=>{}
+  
+  const nexQuestion=()=>{}
   return (
-    <div className="App">
-      hi
+    <div className="app">
+      <h1>Quiz Time</h1>
+      <button className='app__start' onClick={startTrivia}>Start</button>
+    <p className="score">Points:</p>
+    <p>ready?</p>
+    {/* <Card questionNum={number+1}
+    totalQuestions={TOTAL_QUESTIONS}
+    question={questions[number].question}
+    answers={questions[number].answers}
+    userAnswer={userAnswers?userAnswers[number]:undefined}
+    callback={checkAnswer}
+    /> */}
+    <button className='app__next' onClick={nexQuestion}>Next</button>
     </div>
   );
 }
 
 export default App;
+
+//
---
+++ src/components/Card.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNum: number;
+  totalQuestions: number;
+};
+
+const Card: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNum,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p className="card__number">
+        Question:{questionNum}/{totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map((answer) => (
+          <div>
+            <button disabled={userAnswer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
